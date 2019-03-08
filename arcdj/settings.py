960c6bf250c9b561a60f4c62e0072ee8163400d6
Gon Honig <gonhonig@Gons-MacBook-Pro.local>
2019-03-09 01:56:50 +0200

    fourth commit

--- arcdj/settings.py
+++ arcdj/settings.py
@@ -121,9 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 …

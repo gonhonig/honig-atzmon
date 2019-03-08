3c21a3d16b2bdf1338bf0f2ee1f76f337edd6555
Gon Honig <gonhonig@Gons-MacBook-Pro.local>
2019-03-09 01:52:17 +0200

    third commit

--- arcdj/settings.py
+++ arcdj/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+…
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
